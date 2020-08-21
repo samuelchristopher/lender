137a26afd834d7232762fd38232c4d3cdaa11ebe
Sam Chris <thesamchris@gmail.com>
2020-08-21 17:10:15 +0800

    created loan object and updated main methods to use loan object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,29 @@
 public class Main {
-    static int[] amounts = new int[100];
-    static String[] borrowers = new String[100];
-    static boolean[] isPaid = new boolean[100];
-    static double[] interestRates = new double[100];
+
+    static Loan[] loans = new Loan[100];
     static int loansCount = 0;
 
     public static void addLoan(String borrower, int amount, double interestRate){
-        borrowers[loansCount] = borrower;
-        amounts[loansCount] = amount;
-        interestRates[loansCount] = interestRate;
+        Loan loan = new Loan(borrower, amount, interestRate);
+        loans[loansCount] = loan;
         loansCount++;
     }
 
     public static void setAsPaid(String borrower){
         for (int i=0; i< loansCount; i++){
-            if (borrowers[i].equals(borrower)){
-                isPaid[i] = true;
+            Loan loan = loans[i];
+            String loanBorrower = loan.getBorrower();
+            if (loanBorrower.equals(borrower)){
+                loan.setAsPaid();
             }
         }
     }
 
     public static void listLoans(){
         for (int i=0; i< loansCount; i++){
-            if (!isPaid[i]){
-                System.out.println(borrowers[i] + ": " + amounts[i]);
+            Loan loan = loans[i];
+            if (!loan.isPaid()){
+                System.out.println(loan.getBorrower() + ": " + loan.getAmount());
             }
         }
     }
@@ -32,8 +32,9 @@ public class Main {
         double profit = 0;
         for (int i=0; i< loansCount; i++){
             // if the loan was repaid, add the interest to profit
-            if (isPaid[i]){
-                profit = profit + (amounts[i] * interestRates[i]);
+            Loan loan = loans[i];
+            if (loan.isPaid()){
+                profit = profit + (loan.getAmount() * loan.getInterestRate());
             }
         }
         return profit;
