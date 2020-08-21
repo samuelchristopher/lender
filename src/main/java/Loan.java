be2f8e95ffc2414699dff6911f32d0205c7da118
Sam Chris <thesamchris@gmail.com>
2020-08-21 17:20:50 +0800

    add maxLoanAmount to track the max value to loan

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -4,6 +4,8 @@ public class Loan {
     private boolean isPaid;
     private double interestRate;
 
+    private static int maxLoanAmount = 0;
+
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
@@ -25,6 +27,10 @@ public class Loan {
 
     public int getAmount() {
         return amount;
+    }
+
+    public static int getMaxLoanAmount() {
+        return maxLoanAmount;
     }
 
     public double getInterestRate() {
